5377dd3a4753912b891a8a54f93558733ef80c4d
Nico <ndilullo@thacher.org>
2023-02-13 20:00:54 -0800

    add comments/headers, import libraries

---
+++ markovModelling.py
@@ -0,0 +1,9 @@
+'''
+What does this code do?
+
+@authors: apata68 & NicoDiLullo
+
+last modified: 13.02.2023
+'''
+import numpy as np
+import matplotlib as plt
--- wordCount.py
+++ wordCount.py
@@ -1,11 +1,17 @@
 '''
-What does this code do
+What does this code do?
 
 @authors: apata68 & NicoDiLullo
 
-last modified: 12.02.2023
+last modified: 13.02.2023
 '''
+import numpy as np
+import matplotlib as plt
 
 #get an array of every word in data set
 
 #make into set
+
+#do some more stuff
+
+#plot it
